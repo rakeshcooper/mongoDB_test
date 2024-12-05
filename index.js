295566512cdd32d2592cf0d1dffaada24db60a09
rakeshcooper <rakeshmurali097@gmail.com>
2024-12-05 20:18:25 +0530

    update

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 const mongoose = require('mongoose')
+const User = require('./user')
 
 // mongoose.connect("mongodb://localhost:27017/cooper")
-
+run()
 async function run(){
-      try {
-      await mongoose.connect('mongodb+srv://mongo:Coooper%40123@cluster0.qmlhy.mongodb.net/test');
-    } catch (error) {
-      console.log(error);
-    }
-
+  mongoose.connect('mongodb+srv://mongo:Coooper%40123@cluster0.qmlhy.mongodb.net/test');
+  const user = await User.create({name: "Cooper", age: 10})
+  user.name = "cooper naidu"
+  await user.save()
+  
+  // const user = new User({name: "Cooper", age: 10})
+  // await user.save()
+  console.log(user);
+  
 }
 
-run()
+
